e8b8c9e1dd4d8ce4adfca4ae0490456181293695
buffsci-wolverines <robotics@bascs.org>
2016-12-14 10:09:36 -0500

    oneJoystickDrive is working when gamepad1.right_bumper

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BaseDrive.java
@@ -11,101 +11,86 @@ import com.qualcomm.robotcore.util.Range;
 @TeleOp(name = "FTCRobotHardware: Base Drive", group = "Drive")
 
 public class BaseDrive extends LinearOpMode {
-	FTCRobotHardware robot = new FTCRobotHardware();
-	private ElapsedTime runtime = new ElapsedTime();
+    FTCRobotHardware robot = new FTCRobotHardware();
+    private ElapsedTime runtime = new ElapsedTime();
 
-	double clawOffset = 0; // Servo mid position
-	final double CLAW_SPEED = 0.02; // sets rate to move servo
+    double clawOffset = 0; // Servo mid position
+    final double CLAW_SPEED = 0.02; // sets rate to move servo
 
-	@Override
-	public void runOpMode() throws InterruptedException {
-		telemetry.addData("Status", "Initialized");
-		telemetry.update();
+    @Override
+    public void runOpMode() throws InterruptedException {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
 
-		double leftMotorPower;
-		double rightMotorPower;
-		double max;
 
 		/*
-		 * Initialize the hardware variables. The init() method of the hardware
+         * Initialize the hardware variables. The init() method of the hardware
 		 * class does all the work here
 		 */
-		robot.init(hardwareMap);
+        robot.init(hardwareMap);
 
-		// Send telemetry message to signify robot waiting;
-		telemetry.addData("Say", "Hello Dear Driver"); //
-		telemetry.update();
+        // Send telemetry message to signify robot waiting;
+        telemetry.addData("Say", "Hello Dear Driver"); //
+        telemetry.update();
 
-		// Wait for the game to start (driver presses PLAY)
-		waitForStart();
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
 
-		// run until the end of the match (driver presses STOP)
-		while (opModeIsActive()) {
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
 
-			tankDrive();
-			ballPick();
-			ballShoot();
 
-			// Run wheels in POV mode (note: The joystick goes negative when
-			// pushed forwards, so negate it)
-			// In this mode the Left stick moves the robot fwd and back, the
-			// Right stick turns left and right.
-			// leftMotorPower = -gamepad1.left_stick_y + gamepad1.right_stick_x;
-			// rightMotorPower = -gamepad1.left_stick_y -
-			// gamepad1.right_stick_x;
-			//
-			// // Normalize the values so neither exceed +/- 1.0
-			// max = Math.max(Math.abs(leftMotorPower),
-			// Math.abs(rightMotorPower));
-			// if (max > 1.0) {
-			// leftMotorPower /= max;
-			// rightMotorPower /= max;
-			// }
-			// OR
-			// //Clip the power values so that it only goes from -1 to 1
-			// left = Range.clip(left, -1, 1);
-			// right = Range.clip(right, -1, 1);
-			// eg: Run wheels in tank mode (note: The joystick goes negative
-			// when pushed forwards)
 
-			// two joystick control
-			// leftMotorPower = -gamepad1.left_stick_y ;
-			// rightMotorPower = -gamepad1.right_stick_y ;
+            ballPick();
+            ballShoot();
 
-			// Only one joystick control
-			// Get the values from the gamepads
-			// Note: pushing the stick all the way up returns -1,
-			// so we need to reverse the y values
-//			float xValue = gamepad1.left_stick_x;
-//			float yValue = -gamepad1.left_stick_y;
-//
-//			// Calculate the power needed for each motor
-//			float leftPower = yValue + xValue;
-//			float rightPower = yValue - xValue;
+            if(gamepad1.right_bumper) {
+                oneJoystickDrive();
+            }else{
+                tankDrive();
+            }
 
-			// Clip the power values so that it only goes from -1 to 1
-//			leftMotorPower = Range.clip(leftPower, -1, 1);
-//			rightMotorPower = Range.clip(rightPower, -1, 1);
 
-			// reduce the speed of left and right motors
-//			leftMotorPower = leftMotorPower / 2;
-//			rightMotorPower = rightMotorPower / 2;
+            // Run wheels in POV mode (note: The joystick goes negative when
+            // pushed forwards, so negate it)
+            // In this mode the Left stick moves the robot fwd and back, the
+            // Right stick turns left and right.
+            // leftMotorPower = -gamepad1.left_stick_y + gamepad1.right_stick_x;
+            // rightMotorPower = -gamepad1.left_stick_y -
+            // gamepad1.right_stick_x;
+            //
+            // // Normalize the values so neither exceed +/- 1.0
+            // max = Math.max(Math.abs(leftMotorPower),
+            // Math.abs(rightMotorPower));
+            // if (max > 1.0) {
+            // leftMotorPower /= max;
+            // rightMotorPower /= max;
+            // }
+            // OR
+            // //Clip the power values so that it only goes from -1 to 1
+            // left = Range.clip(left, -1, 1);
+            // right = Range.clip(right, -1, 1);
+            // eg: Run wheels in tank mode (note: The joystick goes negative
+            // when pushed forwards)
 
-			telemetry.addData("Status", "Run Time: " + runtime.toString());
-			telemetry.addData("left_stick_y", "info: " + -gamepad1.left_stick_y);
-			telemetry.addData("right_stick_y", "info: " + -gamepad1.right_stick_y);
+            // two joystick control
+            // leftMotorPower = -gamepad1.left_stick_y ;
+            // rightMotorPower = -gamepad1.right_stick_y ;
 
-//			robot.leftMotor.setPower(leftMotorPower);
-//			robot.rightMotor.setPower(rightMotorPower);
 
-			// Use gamepad left & right Bumpers to open and close the claw
-			// if (gamepad1.right_bumper)
-			// clawOffset += CLAW_SPEED;
-			// else if (gamepad1.left_bumper)
-			// clawOffset -= CLAW_SPEED;
+//            telemetry.addData("Status", "Run Time: " + runtime.toString());
+//            telemetry.addData("left_stick_y", "info: " + -gamepad1.left_stick_y);
+//            telemetry.addData("right_stick_y", "info: " + -gamepad1.right_stick_y);
+            telemetry.addData("Status", "gamepad1.right_bumper: " + gamepad1.right_bumper);
 
-			// This code will open and close the gripper with two buttons
-			// using 1 button to open and another to close the gripper
+            // Use gamepad left & right Bumpers to open and close the claw
+            // if (gamepad1.right_bumper)
+            // clawOffset += CLAW_SPEED;
+            // else if (gamepad1.left_bumper)
+            // clawOffset -= CLAW_SPEED;
+
+            // This code will open and close the gripper with two buttons
+            // using 1 button to open and another to close the gripper
 //			if (gamepad1.x) {
 //				robot.leftClaw.setPosition(robot.LEFT_Claw_OPEN_POSITION);
 //				robot.rightClaw.setPosition(robot.RIGHT_Claw_OPEN_POSITION);
@@ -113,15 +98,15 @@ public class BaseDrive extends LinearOpMode {
 //				robot.leftClaw.setPosition(robot.LEFT_Claw_CLOSED_POSITION);
 //				robot.rightClaw.setPosition(robot.RIGHT_Claw_CLOSED_POSITION);
 //			}
-			// else {
-			// // Move both servos to new position. Assume servos are mirror
-			// image of each other.
-			// clawOffset = Range.clip(clawOffset, -0.5, 0.5);
-			// robot.leftClaw.setPosition(robot.MID_SERVO + clawOffset);
-			// robot.rightClaw.setPosition(robot.MID_SERVO - clawOffset);
-			// }
+            // else {
+            // // Move both servos to new position. Assume servos are mirror
+            // image of each other.
+            // clawOffset = Range.clip(clawOffset, -0.5, 0.5);
+            // robot.leftClaw.setPosition(robot.MID_SERVO + clawOffset);
+            // robot.rightClaw.setPosition(robot.MID_SERVO - clawOffset);
+            // }
 
-			// Use gamepad buttons to move arm up (Y) and down (A)
+            // Use gamepad buttons to move arm up (Y) and down (A)
 //			if (gamepad1.y)
 //				robot.leftArmMotor.setPower(robot.ARM_UP_POWER);
 //			else if (gamepad1.a)
@@ -129,43 +114,71 @@ public class BaseDrive extends LinearOpMode {
 //			else
 //				robot.leftArmMotor.setPower(0.0);
 
-			// Send telemetry message to signify robot running;
+            // Send telemetry message to signify robot running;
 //			telemetry.addData("claw", "Offset = %.2f", clawOffset);
 //			telemetry.addData("left", "%.2f", leftMotorPower);
 //			telemetry.addData("right", "%.2f", rightMotorPower);
 
-			telemetry.update();
+            telemetry.update();
 
-			// Pause for metronome tick. 40 mS each cycle = update 25 times a
-			// second.
-			robot.waitForTick(40);
-		}
-	}
+            // Pause for metronome tick. 40 mS each cycle = update 25 times a
+            // second.
+            robot.waitForTick(40);
+        }
+    }
 
-	private void ballShoot() {
-		if (gamepad1.y)
-			robot.ballShooter.setPower(1.0);
-		else
-			robot.ballShooter.setPower(0);
+    private void ballShoot() {
+        if (gamepad1.y)
+            robot.ballShooter.setPower(1.0);
+        else
+            robot.ballShooter.setPower(0);
 
-	}
+    }
 
-	private void ballPick() {
-		if (gamepad1.a){
-			robot.ballPicker.setPower(1.0);
-		}
-		else
-			robot.ballPicker.setPower(0);
+    private void ballPick() {
+        if (gamepad1.a) {
+            robot.ballPicker.setPower(1.0);
+        } else
+            robot.ballPicker.setPower(0);
 
-	}
+    }
 
-	public void tankDrive(){
-		float leftY = -gamepad1.left_stick_y;
-		float rightY = -gamepad1.right_stick_y;
-		robot.leftMotor.setPower(leftY);
-		robot.rightMotor.setPower(rightY);
-	}
+    public void tankDrive() {
+        float leftY = -gamepad1.left_stick_y;
+        float rightY = -gamepad1.right_stick_y;
+        robot.leftMotor.setPower(leftY);
+        robot.rightMotor.setPower(rightY);
+    }
 
+    public void oneJoystickDrive() {
+
+            // Only one joystick control
+            // Get the values from the gamepads
+            // Note: pushing the stick all the way up returns -1,
+            // so we need to reverse the y values
+            float xValue = gamepad1.left_stick_x;
+            float yValue = -gamepad1.left_stick_y;
+
+            //			// Calculate the power needed for each motor
+            float leftPower = yValue + xValue;
+            float rightPower = yValue - xValue;
+
+            // Clip the power values so that it only goes from -1 to 1
+            double leftMotorPower;
+            double rightMotorPower;
+            double max;
+
+            leftMotorPower = Range.clip(leftPower, -1, 1);
+            rightMotorPower = Range.clip(rightPower, -1, 1);
+
+            // reduce the speed of left and right motors
+//        leftMotorPower = leftMotorPower / 2;
+//        rightMotorPower = rightMotorPower / 2;
+            robot.leftMotor.setPower(leftMotorPower);
+            robot.rightMotor.setPower(rightMotorPower);
+
+
+    }
 
 
 }
